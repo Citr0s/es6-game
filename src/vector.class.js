b0f8942a5480cd22a6373fdae6c80592004d1052
Flave <david@flave.co.uk>
2017-03-15 20:29:13 +0000

    Adding a multiplication helper on the vector class so both axes can be multiplied by the same factor easier

--- src/vector.class.js
+++ src/vector.class.js
@@ -8,6 +8,11 @@ class Vector {
         this.x += vector.x;
         this.y += vector.y;
     }
+
+    multiply(factor) {
+        this.x *= factor;
+        this.y *= factor;
+    }
 }
 
 export default Vector;
