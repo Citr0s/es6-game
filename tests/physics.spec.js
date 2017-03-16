6e1cd374bdee0440bd0b2a16a1fb5974bbc28ad2
Milosz Dura <milosz.md@wp.pl>
2017-03-16 18:44:18 +0000

    added a suite of tests for physics class

--- tests/physics.spec.js
+++ tests/physics.spec.js
@@ -1,9 +1,46 @@
 import Physics from '../src/physics.class';
+import Vector from '../src/vector.class';
 
 describe('Physics', function() {
-   it('should initialise gravity correctly', function() {
+
+   it('should initialise kinetic horizontal force correctly', function() {
        let subject = new Physics();
 
-       expect(subject.forces['GRAVITY'].y).toBe(9.8);
+       expect(subject.forces['KINETIC_HORIZONTAL'].x).toBe(0);
+       expect(subject.forces['KINETIC_HORIZONTAL'].y).toBe(0);
    });
+
+    it('should initialise kinetic vertical force correctly', function() {
+        let subject = new Physics();
+
+        expect(subject.forces['KINETIC_VERTICAL'].x).toBe(0);
+        expect(subject.forces['KINETIC_VERTICAL'].y).toBe(0);
+    });
+
+    it('should initialise gravity force correctly', function() {
+        let subject = new Physics();
+
+        expect(subject.forces['GRAVITY'].x).toBe(0);
+        expect(subject.forces['GRAVITY'].y).toBe(9.8);
+    });
+
+    it('should update force correctly', function() {
+        let subject = new Physics();
+
+        subject.updateForce("KINETIC_HORIZONTAL", new Vector(1, 2));
+
+        expect(subject.forces['KINETIC_HORIZONTAL'].x).toBe(1);
+        expect(subject.forces['KINETIC_HORIZONTAL'].y).toBe(2);
+    });
+
+    it('should calculate total force correctly', function() {
+        let subject = new Physics();
+
+        subject.updateForce("KINETIC_HORIZONTAL", new Vector(1, 2));
+        subject.updateForce("KINETIC_VERTICAL", new Vector(3, 4));
+        subject.updateForce("GRAVITY", new Vector(5, 6));
+
+        expect(subject.calculateTotalForce().x).toBe(9);
+        expect(subject.calculateTotalForce().y).toBe(12);
+    });
 });
