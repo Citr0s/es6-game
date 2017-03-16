a20c7dfd0b2e7db1ce5bcbe83f47d63ce63bd13b
Milosz Dura <milosz.md@wp.pl>
2017-03-16 06:59:09 +0000

    split out vector tests to keep one assert per test

--- tests/vector.spec.js
+++ tests/vector.spec.js
@@ -1,10 +1,15 @@
 import Vector from '../src/vector.class';
 
 describe('Vector', function() {
-   it('should add two numbers', function() {
+   it('should initialise x correctly', function() {
        let subject = new Vector(1, 1);
 
        expect(subject.x).toBe(1);
-       expect(subject.y).toBe(1);
    });
+
+    it('should initialise y correctly', function() {
+        let subject = new Vector(1, 1);
+
+        expect(subject.y).toBe(1);
+    });
 });
