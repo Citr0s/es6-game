e734079d60d5c8197dc179004de7419276507a39
Milosz Dura <miloszdura@gmail.com>
2018-04-09 00:46:25 +0100

    objects can now collide with each other once again!

--- src/SceneObjects/Systems/CollisionSystem.ts
+++ src/SceneObjects/Systems/CollisionSystem.ts
@@ -11,6 +11,13 @@ class CollisionSystem implements ISystem {
                 let appearance = <AppearanceComponent>entity.getComponent(ComponentType.APPEARANCE);
                 let canvas: HTMLCanvasElement = <HTMLCanvasElement>document.getElementById('canvas');
 
+                for (let i = 0; i < entities.length; i++) {
+                    if (i === parseInt(key))
+                        continue;
+
+                    this.collide(entities[key], entities[i]);
+                }
+
                 collision.isColliding = this.checkForCollisionsWithBoundaries(transform, appearance, canvas.height, canvas.width);
             }
         }
@@ -19,7 +26,34 @@ class CollisionSystem implements ISystem {
     draw(entities: Entity[]): void {
     }
 
-    checkForCollisionsWithBoundaries(transformation: TransformComponent, appearance: AppearanceComponent, boundaryHeight: number, boundaryWidth: number): boolean {
+    private collide(entityOne: Entity, entityTwo: Entity) {
+        let transformEntityOne = <TransformComponent>entityOne.getComponent(ComponentType.TRANSFORM);
+        let transformEntityTwo = <TransformComponent>entityTwo.getComponent(ComponentType.TRANSFORM);
+
+        if (this.areColliding(entityOne, entityTwo)) {
+            let entityOneInitialVelocity = transformEntityOne.initialVelocity;
+            let entityTwoInitialVelocity = transformEntityTwo.initialVelocity;
+            let entityOneMass = transformEntityOne.mass;
+            let entityTwoMass = transformEntityTwo.mass;
+
+            transformEntityOne.velocity.x = ((entityOneMass - entityTwoMass) / (entityOneMass + entityTwoMass)) * entityOneInitialVelocity.x + ((2 * entityTwoMass) / (entityOneMass + entityTwoMass)) * entityTwoInitialVelocity.x;
+            transformEntityOne.velocity.y = ((entityOneMass - entityTwoMass) / (entityOneMass + entityTwoMass)) * entityOneInitialVelocity.y + ((2 * entityTwoMass) / (entityOneMass + entityTwoMass)) * entityTwoInitialVelocity.y;
+        }
+    }
+
+    private areColliding(entityOne: Entity, entityTwo: Entity) {
+        let transformEntityOne = <TransformComponent>entityOne.getComponent(ComponentType.TRANSFORM);
+        let appearanceEntityOne = <AppearanceComponent>entityOne.getComponent(ComponentType.APPEARANCE);
+        let transformEntityTwo = <TransformComponent>entityTwo.getComponent(ComponentType.TRANSFORM);
+        let appearanceEntityTwo = <AppearanceComponent>entityTwo.getComponent(ComponentType.APPEARANCE);
+
+        return transformEntityOne.position.y < transformEntityTwo.position.y + appearanceEntityTwo.height
+            && transformEntityOne.position.y + appearanceEntityOne.height > transformEntityTwo.position.y
+            && transformEntityOne.position.x < transformEntityTwo.position.x + appearanceEntityTwo.width
+            && transformEntityOne.position.x + appearanceEntityOne.width > transformEntityTwo.position.x;
+    }
+
+    private checkForCollisionsWithBoundaries(transformation: TransformComponent, appearance: AppearanceComponent, boundaryHeight: number, boundaryWidth: number): boolean {
         let isColliding = false;
         if (transformation.position.y + appearance.height > boundaryHeight) {
             transformation.position.y = boundaryHeight - appearance.height;
